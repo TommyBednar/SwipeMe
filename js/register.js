3bea4a9de7f7d94a9f0d0b94e19fded64de3386b
WillEngler <whe1@pitt.edu>
2014-09-18 17:05:32 -0400

    Add script to toggle minimum price visibility in registration form

---
+++ js/register.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+ 
+    $( "#set_min" ).click(function( event ) {
+ 
+ 		console.log("Made it in.")
+        $("#min_price_form").slideToggle();
+ 
+    });
+ 
+});
