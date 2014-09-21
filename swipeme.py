313b672a7768a9e5b58c8e2396077e9b9b169fe4
WillEngler <whe1@pitt.edu>
2014-09-21 17:35:13 -0400

    Can pass form data to backend. Error on swipee form

--- swipeme.py
+++ swipeme.py
@@ -2,7 +2,7 @@ import os
 import webapp2
 import jinja2
 
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 from google.appengine.api import users
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -10,76 +10,81 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class Member(db.Model):
+class User(ndb.Model):
     # Authentication for logging in via Google Accounts API
-    owner = db.UserProperty()
+    google_account = ndb.UserProperty()
 
-    # True if user is offering swipes (Swipee), false if
-    # user is looking for swipes (Swiper)
-    offering = db.BooleanProperty()
+    # "swiper" or "swipee"
+    user_type = ndb.StringProperty()
 
     # User's phone number for texting information
-    phone = db.PhoneNumberProperty()
+    phone_number = ndb.StringProperty()
 
-    # True if user is currently active.  For the Swipee,
-    # this means they are in market.  For the Swiper, it 
+    # True if user is currently active.  For the Swipeer,
+    # this means they are in market.  For the Swipee, it 
     # means they are waiting to be swiped in.
-    active = db.BooleanProperty()
+    is_active = ndb.BooleanProperty()
 
-    # Prices the user is setting.  For the Swipee, this is
-    # the price they are charging.  For the Swiper, this is
-    # the maximum price they will pay.  Will be used in the
-    # selection algorithm.
-    price = db.FloatProperty()
+    # The swiper's asking price.
+    # For now, swipees have no price parameter
+    asking_price = ndb.IntegerProperty()
 
-    @staticmethod
-    def swipers():
-        return 0
-        # Return all users that are swipers (offering is false)
+    @classmethod
+    def create_key(cls, email):
+        return ndb.Key(cls,email)
 
-    @staticmethod
-    def swipees():
-        return 0
-        # Return all users that are swipees (offering is true)
-
+#Render landing page
 class LandingPage(webapp2.RequestHandler):
     def get(self):
         
+        #TODO: Check if user is logged in. If so, redirect to /user/home
 
         #Render the page
         template = JINJA_ENVIRONMENT.get_template("index.html")
         self.response.write(template.render())
 
+class Home(webapp2.RequestHandler):
+    def get(self):
 
-# Temporary handler to display and add users (testing the user model, Swiper)
+        user_key = User.create_key(users.get_current_user().email())
+        user = user_key.get()
+
+        self.response.write('<html><body>')
+        self.response.write(user.user_type)
+        self.response.write('<br>')
+        self.response.write(user.phone_number)
+        self.response.write('</body></html>')
+
+# Temporary handler to display and add users
 class Register(webapp2.RequestHandler):
     def get(self):
 
         user_type = self.request.get('user_type')
 
-        template = JINJA_ENVIRONMENT.get_template("register.html")
+        template = JINJA_ENVIRONMENT.get_template("user/register.html")
         self.response.write(template.render( { 'user_type': user_type} ))
         
 # Handles POST requests to add a new user.  Also temporary, for testing the Member model
 class AddUser(webapp2.RequestHandler):
     def post(self):
-        new_swiper = Member()
+        
+        current_google_account = users.get_current_user()
+        new_user = User(key=User.create_key(current_google_account.email()))
+        
+        new_user.google_account = current_google_account
+        new_user.is_active = False;
+        new_user.user_type = self.request.get('user_type')
+        new_user.phone_number = self.request.get('phone_number')
+        new_user.asking_price = int(self.request.get('asking_price'))
 
-        if users.get_current_user():
-            new_swiper.owner = users.get_current_user()
-
-        new_swiper.offering = (self.request.get('offering') == 'on')
-        new_swiper.active = (self.request.get('active') == 'on')
-        new_swiper.phone = self.request.get('phone')
-        new_swiper.price = float(self.request.get('price'))
-
-        new_swiper.put()
-        self.redirect('/register')
+        new_user.put()
+        
 
 app = webapp2.WSGIApplication([
     ('/', LandingPage),
-    ('/adduser', AddUser),
-    ('/register', Register)
+    ('/user/add_user', AddUser),
+    ('/user/register', Register),
+    ('/user/home', Home)
 ], debug=True)
 
 def main():
