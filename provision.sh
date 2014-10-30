966e4ae0ddaf127d03de3f074eeb910f13db10ef
Tommy Bednar <bednata@gmail.com>
2014-10-30 17:31:56 -0400

    update GAE SDK version

--- provision.sh
+++ provision.sh
@@ -5,6 +5,6 @@
 sudo apt-get update
 sudo apt-get -y install unzip
 
-curl -O https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.12.zip
-unzip google_appengine_1.9.12.zip
+curl -O https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.14.zip
+unzip google_appengine_1.9.14.zip
 echo 'export PATH="$PATH:~/google_appengine"' >> ./.bash_profile
