d65cecbc1b00769b7aa2bdaf121494b13d8c1aa7
Joel Roggeman <joelroggeman@gmail.com>
2014-11-22 13:53:52 -0500

    Fix bug on registration page (customer_type)

--- controllers/register.py
+++ controllers/register.py
@@ -2,6 +2,10 @@ from base_handler import *
 
 class Register(BaseHandler):
     def get(self):
+        customer_type = int(self.request.get('customer_type'))
+
+        if customer_type != 1 and customer_type != 2:
+            self.redirect('/')
 
         template = JINJA_ENVIRONMENT.get_template("customer/register.html")
         self.response.write(template.render( { 'customer_type': self.request.get('customer_type') } ))
