e5a4cce4bfe019cfb8ae5727c9e974786812cdf2
Joel Roggeman <joelroggeman@gmail.com>
2014-11-30 18:50:21 -0500

    Fix registration issues caused by indenting

--- controllers/add_customer.py
+++ controllers/add_customer.py
@@ -17,29 +17,29 @@ class AddCustomer(BaseHandler):
         existing_customer_list = Customer.query(Customer.phone_number == phone).fetch(1)
         if len(existing_customer_list) != 0:
             self.redirect('/')
-
-        # Don't be a jerk and create an infinite loop of messages please
-        if phone == swipeme_globals.PHONE_NUMBER or phone == '2162424434':
-            self.redirect('/')
-
-        new_customer = Customer(key=Customer.create_key(phone))
-
-        new_customer.phone_number = phone
-        new_customer.verification_hash = ''.join(random.choice('0123456789ABCDEF') for i in range(5))
-        new_customer.google_account = users.get_current_user()
-        new_customer.name = new_customer.google_account.nickname()
-        new_customer.email = new_customer.google_account.email()
-        new_customer.customer_type = int(self.request.get('customer_type'))
-
-        #Add customer_type specific data
-        if new_customer.customer_type == Customer.seller:
-            asking_price = int(self.request.get('asking_price'))
-            new_customer.init_seller(asking_price)
         else:
-            new_customer.init_buyer()
+            # Don't be a jerk and create an infinite loop of messages please
+            if phone == swipeme_globals.PHONE_NUMBER or phone == '2162424434':
+                self.redirect('/')
+            else:
+                new_customer = Customer(key=Customer.create_key(phone))
+
+                new_customer.phone_number = phone
+                new_customer.verification_hash = ''.join(random.choice('0123456789ABCDEF') for i in range(5))
+                new_customer.google_account = users.get_current_user()
+                new_customer.name = new_customer.google_account.nickname()
+                new_customer.email = new_customer.google_account.email()
+                new_customer.customer_type = int(self.request.get('customer_type'))
+
+                #Add customer_type specific data
+                if new_customer.customer_type == Customer.seller:
+                    asking_price = int(self.request.get('asking_price'))
+                    new_customer.init_seller(asking_price)
+                else:
+                    new_customer.init_buyer()
 
 
-        new_customer.put()
+                new_customer.put()
 
-        SMSHandler.send_new_verification_message(new_customer)
+                SMSHandler.send_new_verification_message(new_customer)
 
